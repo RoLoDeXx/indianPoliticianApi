40dbcf21f796e956ac4f52f3ebb2fc98cf4a43fe
RoLoDeXx <dexxrolo@gmail.com>
2019-08-18 00:06:13 +0530

    news added

--- src/middlewares/searchBy.js
+++ src/middlewares/searchBy.js
@@ -1,28 +1,45 @@
 const Politician = require("../models/politiciansModel");
+const fetch = require("node-fetch");
 
 const searchBy = async (req, res, next) => {
   let query = req.params.query;
-
   const politician = await Politician.findOne({
     name: query.charAt(0).toUpperCase() + query.slice(1)
   });
   // res.status(200).send(politician);
   if (politician) {
-    console.log(politician);
-    return res.render("profile.hbs", { politician });
+    getData(politician.name).then(media => {
+      // console.log(media.articles);
+      res.render("profile.hbs", { politician, articles: media.articles });
+      // console.log(politician);
+    });
   } else {
     try {
       const politician = await Politician.findOne({
         area: query.toUpperCase()
       });
-      if (politician) return res.render("profile.hbs", { politician });
-      else return res.status(200).send("Be precise when you're naming area");
+      if (politician) {
+        getData(politician.name).then(media => {
+          // console.log(media.articles);
+          res.render("profile.hbs", { politician, articles: media.articles });
+        });
+      } else return res.status(200).send("Be precise when you're naming area");
     } catch (error) {
       return res.status(400).send("No such politician found");
     }
   }
 
   next();
+};
+
+const getData = async name => {
+  let media = await fetch(
+    "https://newsapi.org/v2/everything?q=" +
+      name +
+      "&apiKey=c56342e38b764faa9a491db2132bc3e6&pageSize=5"
+  );
+  media = await media.json();
+  return media;
 };
 
 module.exports = searchBy;
