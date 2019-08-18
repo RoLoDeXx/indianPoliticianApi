f97b7be2ce227cf5c97fe94c3dbce36a5f8275ac
RoLoDeXx <dexxrolo@gmail.com>
2019-08-19 00:12:57 +0530

    another ui update

--- src/middlewares/searchBy.js
+++ src/middlewares/searchBy.js
@@ -36,7 +36,7 @@ const getData = async name => {
   let media = await fetch(
     "https://newsapi.org/v2/everything?q=" +
       name +
-      "&apiKey=c56342e38b764faa9a491db2132bc3e6&pageSize=5"
+      "&apiKey=c56342e38b764faa9a491db2132bc3e6&pageSize=10"
   );
   media = await media.json();
   return media;
