10c6784cd99316ef9c5487e766c92b532d8a6892
RoLoDeXx <dexxrolo@gmail.com>
2019-09-06 21:54:12 +0530

    prod

--- src/db/db.js
+++ src/db/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const urlDB = "mongodb://127.0.0.1:27017/politics-data-api";
+const urlDB = process.env.MONGODB_URL;
 mongoose.connect(urlDB, {
   useNewUrlParser: true,
   useCreateIndex: true
