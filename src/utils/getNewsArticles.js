33b42e17ea98d7077d6fab46c45a6dc3de51c3ef
RoLoDeXx <dexxrolo@gmail.com>
2019-11-18 00:33:32 +0530

    fixed grid ui and updated database

--- src/utils/getNewsArticles.js
+++ src/utils/getNewsArticles.js
@@ -4,7 +4,7 @@ const getNewsArticles = async name => {
   let media = await fetch(
     "https://newsapi.org/v2/everything?q=" +
       name +
-      "&apiKey=c56342e38b764faa9a491db2132bc3e6&pageSize=5"
+      "&apiKey=c56342e38b764faa9a491db2132bc3e6&pageSize=10"
   );
   media = await media.json();
 
--- src/utils/getVideos.js
+++ src/utils/getVideos.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const getVideos = async query => {
   let res = await axios.get(
-    `https://www.googleapis.com/youtube/v3/search?q=${query} politicis&part=snippet&maxResults=5&key=AIzaSyDSrYs3b7R2UA5HvUSQkRQw9gG5B38sPNo`
+    `https://www.googleapis.com/youtube/v3/search?q=${query} politicis&part=snippet&maxResults=10&key=AIzaSyDSrYs3b7R2UA5HvUSQkRQw9gG5B38sPNo`
   );
   return res.data.items;
 };
