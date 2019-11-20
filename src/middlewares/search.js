b50cf479a5d4d368d14f1c068789e2d7b642225f
RoLoDeXx <dexxrolo@gmail.com>
2019-11-20 13:25:20 +0530

    search page not loading bug fixed

--- src/middlewares/search.js
+++ src/middlewares/search.js
@@ -1,18 +1,22 @@
 const Politician = require("../models/politiciansModel");
+const getImage = require("../utils/getImage");
 
 const search = async (req, res, next) => {
-  let username = req.query.searchFeild;
-  if (username === "" || username === undefined) return;
+  let query = req.query.searchFeild;
+  if (query === "" || query === undefined) {
+    res.render("search");
+  } else {
+    let response = await Politician.find({
+      name: { $regex: query, $options: "i" }
+    });
 
-  let response = await Politician.find({
-    name: { $regex: username, $options: "i" }
-  });
-
-  console.log(response);
-
-  res.render("search", {
-    politicians: response
-  });
+    if (response) {
+      res.render("search", {
+        politicians: response
+      });
+    }
+    console.log(response);
+  }
   next();
 };
 
