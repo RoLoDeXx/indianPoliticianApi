8149f33a1a845778325bf6c0769b66ddb10f48b6
RoLoDeXx <dexxrolo@gmail.com>
2019-11-24 18:30:54 +0530

    search page bug fix #2

--- src/middlewares/search.js
+++ src/middlewares/search.js
@@ -2,28 +2,29 @@ const Politician = require("../models/politiciansModel");
 
 const search = async (req, res, next) => {
   let query = req.query.searchFeild;
-  let byArea = req.query.switch;
-
-  if (byArea) {
-    let response = await Politician.find({
-      area: { $regex: query, $options: "i" },
-      year: "19"
-    });
-
-    res.render("search", {
-      matches: response.length,
-      politicians: response
-    });
-  } else {
-    let response = await Politician.find({
-      name: { $regex: query, $options: "i" },
-      year: "19"
-    });
-
-    res.render("search", {
-      matches: response.length,
-      politicians: response
-    });
+  let area = req.query.switch;
+  try {
+    if (area) {
+      let response = await Politician.find({
+        area: { $regex: query, $options: "i" },
+        year: "19"
+      });
+      res.render("search", {
+        matches: response.length,
+        politicians: response
+      });
+    } else {
+      let response = await Politician.find({
+        name: { $regex: query, $options: "i" },
+        year: "19"
+      });
+      res.render("search", {
+        matches: response.length,
+        politicians: response
+      });
+    }
+  } catch (error) {
+    res.render("search");
   }
   next();
 };
